37056b040307c5913b9c2b1802678d3e8abe3daf
LaDfBC <george.mausshardt@gmail.com>
2018-04-08 19:14:30 -0500

    #68 Justification - minor improvements to avoid extraneous lines and function calls

--- src/Justification.java
+++ src/Justification.java
@@ -18,12 +18,11 @@ public class Justification {
             if(currentWordLength + currentLineLength > maxWidth) {
                  output.add(createLine(wordsInLine, maxWidth, currentLineLength, false));
                  wordsInLine = new ArrayList<>();
-                 wordsInLine.add(words[i]);
                  currentLineLength = currentWordLength + 1;
             } else {
-                wordsInLine.add(words[i]);
                 currentLineLength += currentWordLength + 1;
             }
+            wordsInLine.add(words[i]);
         }
 
         output.add(createLine(wordsInLine, maxWidth, currentLineLength, true));
@@ -36,29 +35,30 @@ public class Justification {
                               int currentLineLength,
                               boolean lastLine) {
         StringBuilder builder = new StringBuilder();
+        int size = wordsInLine.size();
         if(lastLine) {
             for(int i = 0; i < wordsInLine.size() - 1; i++) {
                 builder.append(wordsInLine.get(i));
                 builder.append(" ");
             }
-            builder.append(wordsInLine.get(wordsInLine.size() - 1));
+            builder.append(wordsInLine.get(size - 1));
             while(builder.length() < maxWidth) {
                 builder.append(" ");
             }
         } else {
-            if (wordsInLine.size() < 3) {
+            if (size < 3) {
                 builder.append(wordsInLine.get(0));
-                for (int i = currentLineLength - (wordsInLine.size()); i < maxWidth; i++) {
+                for (int i = currentLineLength - (size); i < maxWidth; i++) {
                     builder.append(" ");
                 }
-                if (wordsInLine.size() == 2) {
+                if (size == 2) {
                     builder.append(wordsInLine.get(1));
                 }
             } else {
                 int wordIndex = 0;
-                for (int i = currentLineLength - (wordsInLine.size()); i < maxWidth; i++) {
+                for (int i = currentLineLength - (size); i < maxWidth; i++) {
                     wordsInLine.set(wordIndex, wordsInLine.get(wordIndex).concat(" "));
-                    if (wordIndex == wordsInLine.size() - 2) {
+                    if (wordIndex == size - 2) {
                         wordIndex = 0;
                     } else {
                         wordIndex++;
